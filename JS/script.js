505d0f07f3a94120cfa194a8db92e3aac3bbec84
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-01-23 15:51:11 +0100

    added list items

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,15 @@
-console.log('HELLO WORLD')
+const {createApp} = Vue
+const app = createApp({
+   data(){
+    return  {
+        tasks :[
+            { id: 1, done: false, text: 'Fare la spesa' },
+            { id: 2, done: true, text: 'Lavare i capelli' },
+            { id: 3, done: false, text: 'Comprare una marca da bollo' },
+            { id: 4, done: false, text: 'Aggiornare il PC' }
+          ]
+    }
+   }
+})
+app.mount('#root');
+
