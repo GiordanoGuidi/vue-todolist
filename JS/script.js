f0f38a1de952ba781fcf4bea13b61a3810df7be6
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-01-23 17:19:56 +0100

    added deleteToDo method

--- JS/script.js
+++ JS/script.js
@@ -9,6 +9,18 @@ const app = createApp({
             { id: 4, done: false, text: 'Aggiornare il PC' }
           ]
     }
+   },
+   methods:{
+    //CREO FUNZIONE CHE RIMUOVE I TASK
+    deleteToDo(id){
+      const newTaskArray = this.tasks.filter((task)=>{
+        if(id !==task.id){
+          return true;
+        }else return false;
+      })
+      this.tasks = newTaskArray
+      console.log(this.tasks)
+    }
    }
 })
 app.mount('#root');
