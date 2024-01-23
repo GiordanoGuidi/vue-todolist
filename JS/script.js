534fcd312b40bf107f7b5081c75dc85352acc896
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-01-23 18:53:21 +0100

    added method addNewTask

--- JS/script.js
+++ JS/script.js
@@ -7,8 +7,11 @@ const app = createApp({
             { id: 2, done: true, text: 'Lavare i capelli' },
             { id: 3, done: false, text: 'Comprare una marca da bollo' },
             { id: 4, done: false, text: 'Aggiornare il PC' }
-          ]
+          ],
+          newTask:'',
     }
+   },
+   computed:{
    },
    methods:{
     //CREO FUNZIONE CHE RIMUOVE I TASK
@@ -20,8 +23,19 @@ const app = createApp({
       })
       this.tasks = newTaskArray
       console.log(this.tasks)
+    },
+
+    //CREO FUNZIONE CHE AGGIUNGE UN OGGETTO ALL'ARRAY TASKS
+    addNewTask(){
+     const newTask = {
+       id:5,
+       done:false,
+       text:this.newTask,
+     };
+     this.tasks.push(newTask)
     }
-   }
+   },
+
 })
 app.mount('#root');
 
